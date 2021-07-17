5b63a9c1cd4ab9d730c642aeb9b65135c4a86b96
harshal <harshalogale24@gmail.com>
2021-07-17 20:12:16 +0530

    question added

--- NumberPatter-5.java
+++ NumberPatter-5.java
@@ -1,3 +1,13 @@
+// Pattern programs in java : Pattern 5
+
+// 1 2 3 4 5 6 7
+// 1 2 3 4 5 6
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+
 class Number5Patter
 {
     public static void main(String[] args)
